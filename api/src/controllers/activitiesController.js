dd2356de83a46f2888cdd73abb83c4daeeabcca6
pameluve <pameluve@gmail.com>
2023-05-31 12:08:05 -0400

    refactor (src/controllers/activitiesController.js): countries name info at getActivity

--- api/src/controllers/activitiesController.js
+++ api/src/controllers/activitiesController.js
@@ -37,7 +37,16 @@ const getActivity = async (id)=>{
             through: { attributes:[] }
         }]
     })
-    return activity;
+
+    const theActivity = {
+            id: activity.id,
+            name: activity.name,
+            difficulty: activity.difficulty,
+            duration: activity.duration,
+            season: activity.season,
+            countries: activity.countries.map(ele=>ele.name)
+    }
+    return theActivity;
 };
 
 //----------------------POST ACTIVITIES-----------
