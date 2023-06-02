507cffec29c8ede6ba0ebd2f41970ee6fd33ab8b
pameluve <pameluve@gmail.com>
2023-06-02 17:14:25 -0400

    refactor(../client/src/components/styles/searchBar.css): search by name view

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -6,31 +6,41 @@ import { Link } from "react-router-dom";
 import "./styles/searchBar.css";
 import logo from "./media/logo.gif";
 
-const SearchBar = ({setCurrentPage})=>{
-    const dispatch = useDispatch();
-    const [name, setName] = useState("");
+const SearchBar = ({ setCurrentPage }) => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
 
-    const inputHandler = (event)=>{
-        event.preventDefault();
-        setName(event.target.value)
-    }
+  const inputHandler = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  };
 
-    const submitHandler = (event)=>{
-        event.preventDefault();
-        dispatch(getCountryByName(name));
-        setCurrentPage(1);
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(getCountryByName(name));
+    setCurrentPage(1);
+  };
 
-    return(
-        <div className="searchContainer">
-            <img src={logo} alt=""/>
-            <input type="text" placeholder="Nombre del Pais..." onChange={(event)=> inputHandler(event)}/>
-            <button className="btn" type="submit" onClick={(event)=> submitHandler(event)}>Buscar</button>
-            <Link to ="/activities">
-                <button className="btn">Actividades Turisticas</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="searchContainer">
+      <img src={logo} alt="" />
+      <input
+        type="text"
+        placeholder="Nombre del Pais..."
+        onChange={(event) => inputHandler(event)}
+      />
+      <button
+        className="btn"
+        type="submit"
+        onClick={(event) => submitHandler(event)}
+      >
+        Buscar
+      </button>
+      <Link to="/activities">
+        <button className="btn">Actividades Turisticas</button>
+      </Link>
+    </div>
+  );
 };
 
 export default SearchBar;
